c5bd61b46dfa1c3ee77444bb1a77c54ebf54504e
Eric Dallo <ericdallo06@hotmail.com>
2020-12-30 18:40:03 -0300

    Remove redundant and

--- lsp-treemacs.el
+++ lsp-treemacs.el
@@ -1228,7 +1228,7 @@ With a prefix argument, select the new window expand the tree of implementations
   "Show the incoming call hierarchy for the symbol at point.
 With a prefix argument, show the outgoing call hierarchy."
   (interactive "P")
-  (unless (and (lsp-feature? "textDocument/prepareCallHierarchy"))
+  (unless (lsp-feature? "textDocument/prepareCallHierarchy")
     (user-error "Call hierarchy not supported by the current servers: %s"
                 (-map #'lsp--workspace-print (lsp-workspaces))))
   (let ((buffer (current-buffer)))
